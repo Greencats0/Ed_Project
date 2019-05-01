8f3ff05943a0dd45878d7f375fba2f9df2b022dc
Jake David Wong <43362857+Greencats0@users.noreply.github.com>
2019-05-01 16:25:28 -0700

    Update edgrep.h

--- edgrep.h
+++ edgrep.h
@@ -5,7 +5,25 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
-
+const int BLKSIZE = 4096;  const int NBLK = 2047;  const int FNSIZE = 128;  const int LBSIZE = 4096;
+const int ESIZE = 256; const int GBSIZE = 256;  const int NBRA = 5;  const int KSIZE = 9;  const int CBRA = 1;
+const int CCHR = 2;  const int CDOT = 4;  const int CCL = 6;  const int NCCL = 8;  const int CDOL = 10;
+const int CEOF = 11;  const int CKET = 12;  const int CBACK = 14;  const int CCIRC = 15;  const int STAR = 01;
+const int READ = 0;  const int WRITE = 1;  /* const int EOF = -1; */
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
+int  peekc, lastc, given, ninbuf, io, pflag;
+int  vflag  = 1, oflag, listf, listn, col, tfile  = -1, tline, iblock  = -1, oblock  = -1, ichanged, nleft;
+int  names[26], anymarks, nbra, subnewa, subolda, fchange, wrapp, bpagesize = 20;
+unsigned nlall = 128;  unsigned int  *addr1, *addr2, *dot, *dol, *zero;
+long  count;
+char  Q[] = "", T[] = "TMP", savedfile[FNSIZE], file[FNSIZE], linebuf[LBSIZE], rhsbuf[LBSIZE/2], expbuf[ESIZE+4];
+char  genbuf[LBSIZE], *nextip, *linebp, *globp, *mktemp(char *), tmpXXXXX[50] = "/tmp/eXXXXX";
+char  *tfname, *loc1, *loc2, ibuff[BLKSIZE], obuff[BLKSIZE], WRERR[]  = "WRITE ERROR", *braslist[NBRA], *braelist[NBRA];
+char  line[70];  char  *linp  = line;
+char grepbuf[GBSIZE];
+char cmdbuf[LBSIZE];
 void add(int i);  unsigned int *address(void);  int advance(char *lp, char *ep);
 int append(int (*f)(void), unsigned int *a);  int backref(int i, char *lp);
 void blkio(int b, char *buf, long (*iofcn)(int, void*, unsigned long));
